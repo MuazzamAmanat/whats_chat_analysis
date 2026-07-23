566238a5324c3f75137b424395f2742c47e7e788
MuazzamAmanat <muazzamgil1731@gmail.com>
2026-07-24 03:05:08 +0500

    WhatsApp Chat Analyzer

---
+++ helper.py
@@ -0,0 +1,204 @@
+from urlextract import URLExtract
+url_extractor=URLExtract()
+import matplotlib.pyplot as plt
+import pandas as pd
+import emoji
+
+def fetch_stats(selected_user,df):
+    if selected_user != "Overall" :
+        num_messages=df[df['users']==selected_user].shape[0]     
+        df=df[df['users']==selected_user]
+        media_files=df[df['messages']=='<Media omitted>\n'].shape[0]    
+        
+        words=[]
+        urls=[]
+        for i in df['messages']:
+            words.extend((i.split()))
+            urls.extend(url_extractor.find_urls(i))
+
+        return num_messages,len(words),media_files,len(urls),
+
+    if selected_user=="Overall" :
+        num_messages= df.shape[0]
+        media_files=df[df['messages']=='<Media omitted>\n'].shape[0]    
+
+        words=[]
+        urls=[]
+
+        for i in df['messages']:
+            words.extend((i.split()))
+            urls.extend(url_extractor.find_urls(i))
+
+        return num_messages,len(words),media_files,len(urls)
+
+#fetch_active_users
+def fetch_active_users(df):
+        df = df[df['users'] != 'group_notification']
+
+        active_users=df['users'].value_counts().head()
+
+        percent =(df['users'].value_counts() / df.shape[0]) * 100
+        percent = percent.reset_index()
+        percent.columns = ["User", "Percentage"]
+        return active_users,percent
+
+
+
+
+# create a wordcloud
+
+from wordcloud import WordCloud
+def word_cloud_generator(selected_user, df):
+
+    if selected_user != "Overall":
+        df = df[df['users'] == selected_user]
+
+    df = df[
+        (df['users'] != 'group_notification') &
+        (df['messages'] != '<Media omitted>\n')
+    ]
+
+    # read stopwords
+    with open('romanEng2.csv', 'r') as f:
+        stop_words = set(f.read().splitlines())
+
+    words = []
+
+    for message in df['messages']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+
+    cleaned_text = " ".join(words)
+
+    wc = WordCloud(
+        width=300,
+        height=300,
+        min_font_size=8,
+        background_color='white'
+    )
+
+    wc_generated = wc.generate(cleaned_text)
+
+    return wc_generated
+
+
+
+
+
+
+#select top20 most used words but make sure there wont be any stopwords
+from collections import Counter
+
+def freq_counter(selected_user,df):
+
+    with open('romanEng2.csv', 'r') as f:
+        stop_words = set(f.read().splitlines())
+
+    if selected_user != "Overall":
+        df = df[df['users'] == selected_user]
+
+    df = df[
+        (df['users'] != 'group_notification') &
+        (df['messages'] != '<Media omitted>\n')
+    ]
+
+    words = []
+
+    for message in df['messages']:
+        for word in message.lower().split():
+            if word not in stop_words:
+                words.append(word)
+
+    words_count = Counter(words).most_common(20)
+
+
+    words_df = pd.DataFrame(
+        words_count,
+        columns=["Word", "How Many Times Used"]
+    )
+
+    return words_df
+
+
+# emojis
+def emoji_check(selected_user, df):
+
+    emojis = []
+
+    if selected_user != "Overall":
+        df = df[df['users'] == selected_user]
+
+    for message in df['messages']:
+        for char in message:
+            if emoji.is_emoji(char):
+                emojis.append(char)
+
+    return Counter(emojis).most_common(10)
+
+
+def monthly_chat_report (selected_user, df):
+    if selected_user != 'Overall':
+        df=df[df["users"]== selected_user]
+    df['month_num']=df['date'].dt.month
+    timeline_df = (df.groupby(["year", "month_num", "month"])["messages"].count().reset_index())
+
+    timeline_df = timeline_df.sort_values(["year", "month_num"])
+    time=[]
+    for i in range(timeline_df.shape[0]):
+        time.append(timeline_df['month'][i] + "-" + str(timeline_df['year'][i]))
+    timeline_df['time']=time
+    return timeline_df
+
+
+def daily_chat_report(selected_user, df):
+    if selected_user != "Overall":
+        df = df[df["users"] == selected_user]
+
+    daily_df = (
+        df.groupby("only_date")["messages"]
+        .count()
+        .reset_index()
+    )
+
+    # latest 30 days
+    daily_df = (
+        daily_df.sort_values("only_date", ascending=False)
+        .head(30)
+        .sort_values("only_date")
+    )
+
+    return daily_df
+
+
+
+
+def most_active_day(df):
+    day_df = df.groupby('day_name')['messages'].count().reset_index()
+
+    days = [
+        "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"
+    ]
+
+    day_df['day_name'] = pd.Categorical(
+        day_df['day_name'],
+        categories=days,
+        ordered=True
+    )
+
+    day_df = day_df.sort_values('day_name')
+
+    return day_df
+
+def most_active_month(df):
+    month_df = df.groupby('month')['messages'].count().reset_index()
+    month_df = month_df.sort_values('messages', ascending=False)
+    return month_df
+
+
+def most_active_member(df):
+    member_df = df['users'].value_counts().reset_index()
+    member_df.columns = ['users', 'messages']
+    return member_df
---
+++ whatsapp_chat_analysis_app.py
@@ -0,0 +1,177 @@
+import streamlit as st
+import pandas as pd 
+import preprocessor, helper
+import matplotlib.pyplot as plt
+import emoji
+import plotly.express as px
+
+
+st.set_page_config(layout="wide")
+# st.set_page_config(layout="centered")
+st.sidebar.title("Whatsapp Chat Analysis")
+uploaded_file=st.sidebar.file_uploader("Choose a file ")
+
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode('utf-8')
+    df=preprocessor.preprocess(data)
+    # st.dataframe(df)
+
+    unique_users_list=df['users'].unique().tolist()
+    unique_users_list.remove('group_notification')
+    unique_users_list.sort()
+    unique_users_list.insert(0,"Overall")
+    selected_user = st.sidebar.selectbox("check data wrt individuals",unique_users_list)
+
+
+
+if st.sidebar.button("Analyze Chat"):
+
+
+    st.title("Overall Stats")
+    num_messages,words,media_files,urls= helper.fetch_stats(selected_user,df) 
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+        st.subheader("Total Messages")
+        st.title(num_messages)
+    with col2:
+        st.subheader("Total Words")
+        st.title(words)
+    with col3:
+        st.subheader("Media Files")
+        st.title(media_files)
+    with col4:
+        st.subheader("Links Shared")
+        st.title(urls)
+    st.title("")
+
+
+
+    st.title("Most Active")
+
+    day_df = helper.most_active_day(df)
+    month_df = helper.most_active_month(df)
+    member_df = helper.most_active_member(df)
+
+
+    # Day and Month side by side
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.subheader("Most Active Days")
+
+        fig, ax = plt.subplots()
+        ax.bar(day_df['day_name'], day_df['messages'], color='red')
+        plt.xticks(rotation=45)
+
+        st.pyplot(fig)
+
+
+    with col2:
+        st.subheader("Most Active Months")
+
+        fig, ax = plt.subplots()
+        ax.bar(month_df['month'], month_df['messages'], color='yellow')
+        plt.xticks(rotation=45)
+
+        st.pyplot(fig)
+
+
+    # Member chart below
+    st.subheader("Most Active Members")
+
+    fig, ax = plt.subplots(figsize=(10,4))
+
+    ax.bar(
+        member_df['users'].head(10),
+        member_df['messages'].head(10),
+        color='green'
+    )
+
+    plt.xticks(rotation=45)
+
+    st.pyplot(fig)
+
+
+
+
+
+
+    st.title('Monthly Chat Report')
+    monthly_chat_df=helper.monthly_chat_report(selected_user,df)
+    fig,ax=plt.subplots(figsize=(10,5))
+    ax.plot(monthly_chat_df['time'],monthly_chat_df['messages'])
+    plt.xticks(rotation=60)
+    st.pyplot(fig)
+
+
+    st.title('Daily Chat Report')
+    daily_chat_df = helper.daily_chat_report(selected_user, df)
+    fig, ax = plt.subplots(figsize=(12,4))
+    ax.plot(daily_chat_df['only_date'],daily_chat_df['messages'])
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
+
+
+
+
+    
+    if selected_user == "Overall":
+        active_user,percent=helper.fetch_active_users(df)
+        names=active_user.index
+        values=active_user.values
+        fig, ax = plt.subplots(figsize=(11,7))
+        col1,col2 =  st.columns(2)
+        with col1:
+            st.header("Most Active User")
+            ax.bar(names,values,color='red')
+            plt.xticks(rotation="vertical",fontsize=15)
+            st.pyplot(fig)
+        with col2:
+            st.header("Active Time %")
+            st.table(percent.style.format({"Percentage": "{:.2f}%"}))
+
+
+
+
+    col1, col2= st.columns([1, 3])  
+    with col2: 
+        st.title("Mostly Used Word's Cloud")
+        wc_img=helper.word_cloud_generator(selected_user,df)
+        st.image(wc_img.to_array(),width=700)
+        words_df=helper.freq_counter(selected_user,df)
+
+    st.title("Most Used Words")
+    st.dataframe(words_df)
+    fig, ax = plt.subplots(figsize=(11,7))
+    ax.bar(words_df["Word"],words_df["How Many Times Used"])
+    ax.set_xlabel("Words")
+    ax.set_ylabel("Frequency")
+    ax.set_title("Most Used Words")
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
+
+
+
+
+
+    st.title("Top 10 emojis Used In Chat")
+    emoji_df = pd.DataFrame(helper.emoji_check(selected_user, df),columns=["Emoji", "How many times used?"])
+    col1, col2,  = st.columns(2)
+    with col1:
+        st.dataframe(emoji_df, width=400)
+    with col2:
+        st.bar_chart(emoji_df.set_index('Emoji')['How many times used?'],)
+
+
+
+    top_emoji = emoji_df.head(10)
+    fig = px.pie(
+        top_emoji,
+        values='How many times used?',
+        names='Emoji',
+        title='Most Used Emojis')
+    fig.update_layout(width=800,height=600)
+    st.plotly_chart(fig)
+    
